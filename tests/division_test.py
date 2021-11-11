6dbff2370550d5bea17446e69cf82285ad18ce23
AnnuAnnaJoy <aaj45@njit.edu>
2021-11-11 10:49:08 -0500

    Correcting errors mentioned in travis

--- tests/division_test.py
+++ tests/division_test.py
@@ -6,4 +6,4 @@ def test_division():
     """testing division class"""
     nos = (10.0, 2.0)
     division = Division(nos)
-    assert division.get_result() == 0.05
+    assert division.get_result() == 5.0
