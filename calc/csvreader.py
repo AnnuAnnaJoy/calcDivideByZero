1ad7b4534c8a531a594d6be01b1d37f3a586ebd4
AnnuAnnaJoy <aaj45@njit.edu>
2021-12-06 12:52:35 -0500

    Addition updated

--- calc/csvreader.py
+++ calc/csvreader.py
@@ -6,6 +6,7 @@ from calc.calculator import Calculator
 class CsvManager:
     if os.listdir(os.getcwd()):
         df = pd.read_csv('smallset.csv', dtype={"value1": int, "value2": int})
-        my_tuple = (df['value1'][0], df['value2'][0])
-        Calculator.add_number(my_tuple)
-        print(Calculator.get_last_result_value())
+        for i in range(0, 10, 1):
+            my_tuple = (df['value1'][i], df['value2'][i])
+            Calculator.add_number(my_tuple)
+            print(Calculator.get_last_result_value())
